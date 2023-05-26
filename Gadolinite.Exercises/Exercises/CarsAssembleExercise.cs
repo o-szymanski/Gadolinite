d303f3621d62408de05dbc1efc193473b26332a7
o-szymanski <osk.szymanski@gmail.com>
2023-05-26 12:34:18 +0200

    Add New Exercise

---
+++ Gadolinite.Exercises/Exercises/CarsAssembleExercise.cs
@@ -0,0 +1,28 @@
+﻿namespace Gadolinite.Exercises.Exercises
+{
+    /* 
+    In this exercise you'll be writing code to analyze the production of an assembly line in a car factory. The assembly line's speed can range from 0 (off) to 10 (maximum).
+    At its lowest speed (1), 221 cars are produced each hour. The production increases linearly with the speed. So with the speed set to 4, it should produce 4 * 221 = 884 cars per hour. 
+    However, higher speeds increase the likelihood that faulty cars are produced, which then have to be discarded. You have three tasks.
+    The following table shows how speed influences the success rate: 0: 0% success rate. 1 to 4: 100% success rate. 5 to 8: 90% success rate. 9: 80% success rate. 10: 77% success rate.
+    */
+
+    public static class CarsAssembleExercise
+    {
+        private const int BaseCarProductionPerOneHour = 221;
+
+        public static double SuccessRate(int speed) => speed switch
+        {
+            0 => 0.00,
+            1 or 2 or 3 or 4 => 1.00,
+            5 or 6 or 7 or 8 => 0.90,
+            9 => 0.80,
+            10 => 0.77,
+            _ => 0
+        };
+
+        public static double ProductionRatePerHour(int speed) => speed * BaseCarProductionPerOneHour * SuccessRate(speed);
+
+        public static int WorkingItemsPerMinute(int speed) => (int)ProductionRatePerHour(speed) / 60;
+    }
+}
---
+++ Gadolinite.Tests/Exercises/CarsAssembleTest.cs
@@ -0,0 +1,121 @@
+﻿using Gadolinite.Exercises.Exercises;
+
+namespace Gadolinite.Tests.Exercises
+{
+    public class CarsAssembleTest
+    {
+        // Calculate the success rate
+
+        [Fact]
+        public void SuccessRateForSpeedZero()
+        {
+            Assert.Equal(0.0, CarsAssembleExercise.SuccessRate(0), precision: 1);
+        }
+
+        [Fact]
+        public void SuccessRateForSpeedOne()
+        {
+            Assert.Equal(1.0, CarsAssembleExercise.SuccessRate(1), precision: 1);
+        }
+
+        [Fact]
+        public void SuccessRateForSpeedFour()
+        {
+            Assert.Equal(1.0, CarsAssembleExercise.SuccessRate(4), precision: 1);
+        }
+
+        [Fact]
+        public void SuccessRateForSpeedFive()
+        {
+            Assert.Equal(0.9, CarsAssembleExercise.SuccessRate(5), precision: 1);
+        }
+
+        [Fact]
+        public void SuccessRateForSpeedNine()
+        {
+            Assert.Equal(0.8, CarsAssembleExercise.SuccessRate(9), precision: 1);
+        }
+
+        [Fact]
+        public void SuccessRateForSpeedTen()
+        {
+            Assert.Equal(0.77, CarsAssembleExercise.SuccessRate(10), precision: 2);
+        }
+
+        // Calculate the production rate per hour
+
+        [Fact]
+        public void ProductionRatePerHourForSpeedZero()
+        {
+            Assert.Equal(0.0, CarsAssembleExercise.ProductionRatePerHour(0), precision: 1);
+        }
+
+        [Fact]
+        public void ProductionRatePerHourForSpeedOne()
+        {
+            Assert.Equal(221.0, CarsAssembleExercise.ProductionRatePerHour(1), precision: 1);
+        }
+
+        [Fact]
+        public void ProductionRatePerHourForSpeedFour()
+        {
+            Assert.Equal(884.0, CarsAssembleExercise.ProductionRatePerHour(4), precision: 1);
+        }
+
+        [Fact]
+        public void ProductionRatePerHourForSpeedSeven()
+        {
+            Assert.Equal(1392.3, CarsAssembleExercise.ProductionRatePerHour(7), precision: 1);
+        }
+
+        [Fact]
+        public void ProductionRatePerHourForSpeedNine()
+        {
+            Assert.Equal(1591.2, CarsAssembleExercise.ProductionRatePerHour(9), precision: 1);
+        }
+
+        [Fact]
+        public void ProductionRatePerHourForSpeedTen()
+        {
+            Assert.Equal(1701.7, CarsAssembleExercise.ProductionRatePerHour(10), precision: 1);
+        }
+
+        // Calculate the number of working items produced per minute
+
+        [Fact]
+        public void WorkingItemsPerMinuteforSpeedZero()
+        {
+            Assert.Equal(0, CarsAssembleExercise.WorkingItemsPerMinute(0));
+        }
+
+        [Fact]
+        public void WorkingItemsPerMinuteforSpeedOne()
+        {
+            Assert.Equal(3, CarsAssembleExercise.WorkingItemsPerMinute(1));
+        }
+
+        [Fact]
+        public void WorkingItemsPerMinuteforSpeedFive()
+        {
+            Assert.Equal(16, CarsAssembleExercise.WorkingItemsPerMinute(5));
+        }
+
+        [Fact]
+        public void WorkingItemsPerMinuteforSpeedEight()
+        {
+            Assert.Equal(26, CarsAssembleExercise.WorkingItemsPerMinute(8));
+        }
+
+        [Fact]
+        public void WorkingItemsPerMinuteforSpeedNine()
+        {
+            Assert.Equal(26, CarsAssembleExercise.WorkingItemsPerMinute(9));
+        }
+
+        [Fact]
+        public void WorkingItemsPerMinuteforSpeedTen()
+        {
+            Assert.Equal(28, CarsAssembleExercise.WorkingItemsPerMinute(10));
+        }
+    }
+}
