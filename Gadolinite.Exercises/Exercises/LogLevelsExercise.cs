5870befb9507276fccc2ba152088eb6cda56d32d
o-szymanski <osk.szymanski@gmail.com>
2023-06-19 18:28:40 +0200

    Add New Exercise

---
+++ Gadolinite.Exercises/Exercises/LogLevelsExercise.cs
@@ -0,0 +1,13 @@
+﻿namespace Gadolinite.Exercises.Exercises
+{
+    /*    
+    In this exercise you'll be processing log-lines. Each log line is a string formatted as follows: "[<LEVEL>]: <MESSAGE>". There are three different log levels: INFO WARNING ERROR
+    You have three tasks, each of which will take a log line and ask you to do something with it.
+    */
+    public static class LogLevelsExercise
+    {
+        public static string Message(string logLine) => logLine[(logLine.IndexOf(':') + 1)..].Trim();
+        public static string LogLevel(string logLine) => logLine[1..logLine.IndexOf(']')].ToLower();
+        public static string Reformat(string logLine) => $"{Message(logLine)} ({LogLevel(logLine)})";
+    }
+}
---
+++ Gadolinite.Tests/Exercises/LogLevelTest.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel;
+using Gadolinite.Exercises.Exercises;
+
+namespace Gadolinite.Tests.Exercises
+{
+    public class LogLevelTest
+    {
+        // Get message from a log line
+
+        [Fact]
+        public void ErrorMessage()
+        {
+            Assert.Equal("Stack overflow", LogLevelsExercise.Message("[ERROR]: Stack overflow"));
+        }
+
+        [Fact]
+        public void WarningMessage()
+        {
+            Assert.Equal("Disk almost full", LogLevelsExercise.Message("[WARNING]: Disk almost full"));
+        }
+
+        [Fact]
+        public void InfoMessage()
+        {
+            Assert.Equal("File moved", LogLevelsExercise.Message("[INFO]: File moved"));
+        }
+
+        [Fact]
+        public void MessageWithLeadingAndTrailingWhiteSpace()
+        {
+            Assert.Equal("Timezone not set", LogLevelsExercise.Message("[WARNING]:   \tTimezone not set  \r\n"));
+        }
+
+        // Get log level from a log line
+
+        [Fact]
+        public void ErrorLogLevel()
+        {
+            Assert.Equal("error", LogLevelsExercise.LogLevel("[ERROR]: Disk full"));
+        }
+
+        [Fact]
+        public void WarningLogLevel()
+        {
+            Assert.Equal("warning", LogLevelsExercise.LogLevel("[WARNING]: Unsafe password"));
+        }
+
+        [Fact]
+        public void InfoLogLevel()
+        {
+            Assert.Equal("info", LogLevelsExercise.LogLevel("[INFO]: Timezone changed"));
+        }
+
+        // Reformat a log line
+
+        [Fact]
+        public void ErrorReformat()
+        {
+            Assert.Equal("Segmentation fault (error)", LogLevelsExercise.Reformat("[ERROR]: Segmentation fault"));
+        }
+
+        [Fact]
+        public void WarningReformat()
+        {
+            Assert.Equal("Decreased performance (warning)", LogLevelsExercise.Reformat("[WARNING]: Decreased performance"));
+        }
+
+        [Fact]
+        public void InfoReformat()
+        {
+            Assert.Equal("Disk defragmented (info)", LogLevelsExercise.Reformat("[INFO]: Disk defragmented"));
+        }
+
+        [Fact]
+        public void ReformatWithLeadingAndTrailingWhiteSpace()
+        {
+            Assert.Equal("Corrupt disk (error)", LogLevelsExercise.Reformat("[ERROR]: \t Corrupt disk\t \t \r\n"));
+        }
+    }
+}
